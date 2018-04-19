31c23fede1ab13962088106ac6869dd20f62e16a
Shaozhen Ding <dsz0111@gmail.com>
2018-04-19 17:23:56 -0400

    add restore from bbr

---
+++ tasks/bbr-restore-director/task.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eu
+
+. "$(dirname $0)"/export-director-metadata
+
+om curl -p /api/v0/deployed/director/credentials/bbr_ssh_credentials > bbr_keys.json
+BOSH_PRIVATE_KEY=$(jq -r '.credential.value.private_key_pem' bbr_keys.json)
+
+pushd director-backup-artifact
+  ../binary/bbr director --host "${BOSH_ADDRESS}" \
+  --username bbr \
+  --private-key-path <(echo "${BBR_PRIVATE_KEY}") \
+  restore \
+  --artifact-path
+
+popd
