930cb0cf78fd60a6140838eaa9bb0dd715c6407c
Shaozhen Ding <dsz0111@gmail.com>
2018-04-20 05:52:47 -0400

    fix

--- tasks/bbr-restore-director/task.sh
+++ tasks/bbr-restore-director/task.sh
@@ -5,7 +5,7 @@
 om_cmd curl -p /api/v0/deployed/director/credentials/bbr_ssh_credentials > bbr_keys.json
 BOSH_PRIVATE_KEY=$(jq -r '.credential.value.private_key_pem' bbr_keys.json)
 
-../binary/bbr director --host "${BOSH_ADDRESS}" \
+./binary/bbr director --host "${BOSH_ADDRESS}" \
 --username bbr \
 --private-key-path <(echo "${BBR_PRIVATE_KEY}") \
 restore \
