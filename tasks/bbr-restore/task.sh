91415375cc16807390989e6764efd8cf7c6c3433
Shaozhen Ding <dsz0111@gmail.com>
2018-04-21 20:25:35 -0500

    fix

--- tasks/bbr-restore/task.sh
+++ tasks/bbr-restore/task.sh
@@ -7,6 +7,7 @@ if [ "$BBR_OPERATION" == "director" ]; then
 	command="director"
 elif [ "$BBR_OPERATION" == "deployment" ]; then
   PAS_DEPLOYMENT="$(om_cmd curl -s -p /api/v0/deployed/products | grep cf-)"
+  echo "restore deployment: $PAS_DEPLOYMENT"
 	command="deployment -d $PAS_DEPLOYMENT"
 else
   echo "BBR_OPERATION can either be deployment or director"
@@ -16,7 +17,7 @@ fi
 om_cmd curl -p /api/v0/deployed/director/credentials/bbr_ssh_credentials > bbr_keys.json
 BOSH_PRIVATE_KEY=$(jq -r '.credential.value.private_key_pem' bbr_keys.json)
 
-./binary/bbr director --host "${BOSH_ADDRESS}" \
+./binary/bbr $command --host "${BOSH_ADDRESS}" \
 --username bbr \
 --private-key-path <(echo "${BBR_PRIVATE_KEY}") \
 restore \
