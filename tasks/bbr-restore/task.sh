c686f50dd38b272cbe38b881e034edbe76f133d8
Shaozhen Ding <dsz0111@gmail.com>
2018-04-21 19:56:44 -0500

    add restore pas

--- tasks/bbr-restore/task.sh
+++ tasks/bbr-restore/task.sh
@@ -2,6 +2,17 @@
 
 . "$(dirname $0)"/export-director-metadata
 
+command=""
+if [ "$BBR_OPERATION" == "director" ]; then
+	command="director"
+elif [ "$BBR_OPERATION" == "deployment" ]; then
+  PAS_DEPLOYMENT="$(om_cmd curl -s -p /api/v0/deployed/products | grep cf-)"
+	command="deployment -d $PAS_DEPLOYMENT"
+else
+  echo "BBR_OPERATION can either be deployment or director"
+  exit 1
+fi
+
 om_cmd curl -p /api/v0/deployed/director/credentials/bbr_ssh_credentials > bbr_keys.json
 BOSH_PRIVATE_KEY=$(jq -r '.credential.value.private_key_pem' bbr_keys.json)
 
