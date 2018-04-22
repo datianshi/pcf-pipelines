ca8037b620185585469e78c38938f83659609f91
Shaozhen Ding <dsz0111@gmail.com>
2018-04-21 21:45:38 -0500

    fix

--- tasks/bbr-restore/task.sh
+++ tasks/bbr-restore/task.sh
@@ -6,9 +6,9 @@ set -x
 
 command=""
 if [ "$BBR_OPERATION" == "director" ]; then
-	command="director --host ${BOSH_ADDRESS}"
+	command="director --host ${BOSH_ADDRESS} --username bbr --private-key-path <(echo ${BBR_PRIVATE_KEY}) "
 elif [ "$BBR_OPERATION" == "deployment" ]; then
-	command="deployment -d ${ERT_DEPLOYMENT_NAME} -t ${BOSH_ADDRESS} --ca-cert ${BOSH_CA_CERT_PATH}"
+	command="deployment -d ${ERT_DEPLOYMENT_NAME} -t ${BOSH_ADDRESS} --ca-cert ${BOSH_CA_CERT_PATH} -u ${BOSH_CLIENT} -p ${BOSH_CLIENT_SECRET}"
 else
   echo "BBR_OPERATION can either be deployment or director"
   exit 1
@@ -18,7 +18,5 @@ om_cmd curl -p /api/v0/deployed/director/credentials/bbr_ssh_credentials > bbr_k
 BOSH_PRIVATE_KEY=$(jq -r '.credential.value.private_key_pem' bbr_keys.json)
 
 ./binary/bbr ${command} \
---username bbr \
---private-key-path <(echo "${BBR_PRIVATE_KEY}") \
 restore \
 --artifact-path ./archive
